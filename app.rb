b9e911528e51375713546584061591449b7df9a4
Nick Pizzigati <npizzigati@gmail.com>
2022-08-02 22:52:05 -0500

    Add failure page and try again option

--- app.rb
+++ app.rb
@@ -15,24 +15,28 @@ get "/" do
 end
 
 post '/upload' do
-    tempfile = params[:file][:tempfile]
-    mail = Mail.read(tempfile.path)
-    html = ''
-    success = true
-    begin
-      html = mail.decoded
-    rescue
-      success = false
-    end
+  tempfile = params[:file][:tempfile]
+  mail = Mail.read(tempfile.path)
+  html = ''
+  success = true
+  begin
+    html = mail.decoded
+  rescue
+    success = false
+  end
 
-    if html.length == 0
-      success = false
-    end
+  if html.length == 0
+    success = false
+  end
 
-    if !success
-      return 'Unable to extract html. Did you upload an .eml file?'
-    end
+  if !success
+    redirect '/failure'
+  end
 
-    headers['Content-Disposition'] = "attachment;filename=good_html_file.html"
-    mail.decoded
+  headers['Content-Disposition'] = "attachment;filename=good_html_file.html"
+  mail.decoded
+end
+
+get '/failure' do
+  erb :failure
 end
